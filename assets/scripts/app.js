e0d62c2c29c19d985d6fbd8cce79607a91021782
Ralph van Schaik <ralph.van.schaik@me.com>
2020-12-15 17:40:56 +0100

    249 static methods header werkt

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,9 +15,9 @@ class Product  {
 class ShoppingCart {
     items = [];
 
-    addProduct() {
+    addProduct(product) {
         this.items.push(product);
-        this.totalOutput = `<h2>Total: \$${1}</h2>`;
+        this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
         }
 
     render() {
@@ -39,8 +39,7 @@ class ProductItem {
     }
 
     addToCart() {
-        console.log('Adding product to cart');
-        console.log(this.product);
+        App.addProductToCart(this.product);
     }
 
     render() {
@@ -95,15 +94,26 @@ class ProductList {
 class Shop {
     render() {
         const renderHook = document.getElementById('app');
-        const cart = new ShoppingCart();
-        const cartEl = cart.render();
+        this.cart = new ShoppingCart();
+        const cartEl = this.cart.render();
         const productList = new ProductList();
         const prodListEl = productList.render();
-
         renderHook.append(cartEl);
         renderHook.append(prodListEl);
     }
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+
+    static init() {
+        const shop = new Shop();
+        shop.render();
+        this.cart = shop.cart;
+    }
+
+    static addProductToCart(product) {
+        this.cart.addProduct(product);
+    }
+}
+
+App.init();
